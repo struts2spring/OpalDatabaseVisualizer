cbe6891b9f033a7135d7afc7c6625212ab2a7d12
struts2spring <struts2spring@gmail.com>
2016-12-30 18:51:06 +0530

    Update ResultListPanel.py

--- src/view/worksheet/ResultListPanel.py
+++ src/view/worksheet/ResultListPanel.py
@@ -1,6 +1,10 @@
 
 import wx
 import wx.dataview as dv
+import os
+from src.view.Constant import ID_RUN,ID_EXECUTE_SCRIPT, ID_RESULT_REFRESH,\
+    ID_ROW_ADD, ID_ROW_DELETE, ID_RESULT_NEXT, ID_RESULT_PREVIOUS,\
+    ID_APPLY_CHANGE, ID_RESULT_FIRST, ID_RESULT_LAST
 
 #----------------------------------------------------------------------
 
@@ -167,11 +171,11 @@ class ResultPanel(wx.Panel):
         b3 = wx.Button(self, label="Delete Row(s)")
         self.Bind(wx.EVT_BUTTON, self.OnDeleteRows, b3)
 
-        btnbox = wx.BoxSizer(wx.HORIZONTAL)
-        btnbox.Add(b1, 0, wx.LEFT|wx.RIGHT, 5)
-        btnbox.Add(b2, 0, wx.LEFT|wx.RIGHT, 5)
-        btnbox.Add(b3, 0, wx.LEFT|wx.RIGHT, 5)
-        self.Sizer.Add(btnbox, 0, wx.TOP|wx.BOTTOM, 5)
+#         btnbox = wx.BoxSizer(wx.HORIZONTAL)
+#         btnbox.Add(b1, 0, wx.LEFT|wx.RIGHT, 5)
+#         btnbox.Add(b2, 0, wx.LEFT|wx.RIGHT, 5)
+#         btnbox.Add(b3, 0, wx.LEFT|wx.RIGHT, 5)
+        self.Sizer.Add(self.constructBottomResultToolBar(), 0, wx.TOP|wx.BOTTOM, 5)
 
         # Bind some events so we can see what the DVC sends us
         self.Bind(dv.EVT_DATAVIEW_ITEM_EDITING_DONE, self.OnEditingDone, self.dvc)
@@ -210,7 +214,33 @@ class ResultPanel(wx.Panel):
 
     def OnValueChanged(self, evt):
         self.log.write("OnValueChanged\n")
+        
+    def constructBottomResultToolBar(self):
+        
+        # create some toolbars
+        tb1 = wx.ToolBar(self, -1, wx.DefaultPosition, wx.DefaultSize,
+                         wx.TB_FLAT | wx.TB_NODIVIDER)
+        tb1.SetToolBitmapSize(wx.Size(16, 16))
+        playImage = None
+        if "worksheet" == os.path.split(os.getcwd())[-1:][0]:
+            imageLocation = os.path.join("..", "..", "images")
 
+        elif "view" == os.path.split(os.getcwd())[-1:][0]:
+            imageLocation = os.path.join("..", "images")
+        tb1.AddLabelTool(id=ID_ROW_ADD, label="Row add", shortHelp="Row add ", bitmap=wx.Bitmap(os.path.join(imageLocation, "row_add.png")))
+        tb1.AddLabelTool(id=ID_ROW_DELETE, label="Row delete", shortHelp="Row delete ", bitmap=wx.Bitmap(os.path.join(imageLocation, "row_delete.png")))
+        tb1.AddLabelTool(id=ID_APPLY_CHANGE, label="Apply data change", shortHelp="Apply data change ", bitmap=wx.Bitmap(os.path.join(imageLocation, "accept.png")))
+        tb1.AddSeparator()
+        tb1.AddLabelTool(id=ID_RESULT_REFRESH, label="Result refresh", shortHelp="Result refresh ", bitmap=wx.Bitmap(os.path.join(imageLocation, "resultset_refresh.png")))
+        tb1.AddLabelTool(id=ID_RESULT_FIRST, label="Result first", shortHelp="Result first ", bitmap=wx.Bitmap(os.path.join(imageLocation, "resultset_first.png")))
+        tb1.AddLabelTool(id=ID_RESULT_NEXT, label="Result next", shortHelp="Result next ", bitmap=wx.Bitmap(os.path.join(imageLocation, "resultset_next.png")))
+        tb1.AddLabelTool(id=ID_RESULT_PREVIOUS, label="Result previous", shortHelp="Result previous ", bitmap=wx.Bitmap(os.path.join(imageLocation, "resultset_previous.png")))
+        tb1.AddLabelTool(id=ID_RESULT_LAST, label="Result last", shortHelp="Result last ", bitmap=wx.Bitmap(os.path.join(imageLocation, "resultset_last.png")))
+        tb1.AddSeparator()
+
+        tb1.Realize()
+        
+        return tb1 
         
 #----------------------------------------------------------------------
 
@@ -227,77 +257,122 @@ class ResultPanel(wx.Panel):
 #     return win
 
 
+#         sizer.Fit(self)
+class CreatingResultWithToolbarPanel(wx.Panel):
+    def __init__(self, parent=None, *args, **kw):
+        wx.Panel.__init__(self, parent, id=-1)
+        self.parent = parent
+        
+        vBox = wx.BoxSizer(wx.VERTICAL)
 
+        ####################################################################
+        topResultToolbar = self.constructTopResultToolBar()
+        resultPanel = ResultPanel(self, data=self.getData())
+#         bottomResultToolbar = self.constructBottomResultToolBar()
+        
+        ####################################################################
+        vBox.Add(topResultToolbar , 0, wx.EXPAND | wx.ALL, 0)
+        vBox.Add(resultPanel , 1, wx.EXPAND | wx.ALL, 0)
+#         vBox.Add(bottomResultToolbar , 0, wx.EXPAND | wx.ALL, 0)
+#         vBox.Add(resultPanel , 1, wx.EXPAND | wx.ALL)
+        sizer = wx.BoxSizer(wx.VERTICAL)
+#         sizer.Add(worksheetToolbar ,.9, wx.EXPAND | wx.ALL, 0)
+        sizer.Add(vBox, 1, wx.EXPAND , 0)
+        self.SetSizer(sizer)    
+        
+    def constructTopResultToolBar(self):
+        
+        # create some toolbars
+        tb1 = wx.ToolBar(self, -1, wx.DefaultPosition, wx.DefaultSize,
+                         wx.TB_FLAT | wx.TB_NODIVIDER)
+        tb1.SetToolBitmapSize(wx.Size(16, 16))
+        playImage = None
+        if "worksheet" == os.path.split(os.getcwd())[-1:][0]:
+            imageLocation = os.path.join("..", "..", "images")
 
+        elif "view" == os.path.split(os.getcwd())[-1:][0]:
+            imageLocation = os.path.join("..", "images")
+        tb1.AddLabelTool(id=ID_RUN, label="Run", shortHelp="run single line ", bitmap=wx.Bitmap(os.path.join(imageLocation, "sql_exec.png")))
+        tb1.AddLabelTool(id=ID_EXECUTE_SCRIPT, label="Run Script", shortHelp="execute script ", bitmap=wx.Bitmap(os.path.join(imageLocation, "sql_script_exec.png")))
+        tb1.AddSeparator()
+
+        tb1.Realize()
+        
+        return tb1     
+  
+    def getData(self):
+        # Get the data from the ListCtrl sample to play with, converting it
+        # from a dictionary to a list of lists, including the dictionary key
+        # as the first element of each sublist.
+        musicdata = {
+        1 : ("Bad English", "The Price Of Love", "Rock"),
+        2 : ("DNA featuring Suzanne Vega", "Tom's Diner", "Rock"),
+        3 : ("George Michael", "Praying For Time", "Rock"),
+        4 : ("Gloria Estefan", "Here We Are", "Rock"),
+        5 : ("Linda Ronstadt", "Don't Know Much", "Rock"),
+    #     6 : ("Michael Bolton", "How Am I Supposed To Live Without You", "Blues"),
+    #     7 : ("Paul Young", "Oh Girl", "Rock"),
+    #     8 : ("Paula Abdul", "Opposites Attract", "Rock"),
+    #     9 : ("Richard Marx", "Should've Known Better", "Rock"),
+    #     10: ("Rod Stewart", "Forever Young", "Rock"),
+    #     11: ("Roxette", "Dangerous", "Rock"),
+    #     12: ("Sheena Easton", "The Lover In Me", "Rock"),
+    #     13: ("Sinead O'Connor", "Nothing Compares 2 U", "Rock"),
+    #     14: ("Stevie B.", "Because I Love You", "Rock"),
+    #     15: ("Taylor Dayne", "Love Will Lead You Back", "Rock"),
+    #     16: ("The Bangles", "Eternal Flame", "Rock"),
+    #     17: ("Wilson Phillips", "Release Me", "Rock"),
+    #     18: ("Billy Joel", "Blonde Over Blue", "Rock"),
+    #     19: ("Billy Joel", "Famous Last Words", "Rock"),
+    #     20: ("Janet Jackson", "State Of The World", "Rock"),
+    #     21: ("Janet Jackson", "The Knowledge", "Rock"),
+    #     22: ("Spyro Gyra", "End of Romanticism", "Jazz"),
+    #     23: ("Spyro Gyra", "Heliopolis", "Jazz"),
+    #     24: ("Spyro Gyra", "Jubilee", "Jazz"),
+    #     25: ("Spyro Gyra", "Little Linda", "Jazz"),
+    #     26: ("Spyro Gyra", "Morning Dance", "Jazz"),
+    #     27: ("Spyro Gyra", "Song for Lorraine", "Jazz"),
+    #     28: ("Yes", "Owner Of A Lonely Heart", "Rock"),
+    #     29: ("Yes", "Rhythm Of Love", "Rock"),
+    #     30: ("Billy Joel", "Lullabye (Goodnight, My Angel)", "Rock"),
+    #     31: ("Billy Joel", "The River Of Dreams", "Rock"),
+    #     32: ("Billy Joel", "Two Thousand Years", "Rock"),
+    #     33: ("Janet Jackson", "Alright", "Rock"),
+    #     34: ("Janet Jackson", "Black Cat", "Rock"),
+    #     35: ("Janet Jackson", "Come Back To Me", "Rock"),
+    #     36: ("Janet Jackson", "Escapade", "Rock"),
+    #     37: ("Janet Jackson", "Love Will Never Do (Without You)", "Rock"),
+    #     38: ("Janet Jackson", "Miss You Much", "Rock"),
+    #     39: ("Janet Jackson", "Rhythm Nation", "Rock"),
+    #     40: ("Cusco", "Dream Catcher", "New Age"),
+    #     41: ("Cusco", "Geronimos Laughter", "New Age"),
+    #     42: ("Cusco", "Ghost Dance", "New Age"),
+    #     43: ("Blue Man Group", "Drumbone", "New Age"),
+    #     44: ("Blue Man Group", "Endless Column", "New Age"),
+    #     45: ("Blue Man Group", "Klein Mandelbrot", "New Age"),
+    #     46: ("Kenny G", "Silhouette", "Jazz"),
+    #     47: ("Sade", "Smooth Operator", "Jazz"),
+    #     48: ("David Arkenstone", "Papillon (On The Wings Of The Butterfly)", "New Age"),
+    #     49: ("David Arkenstone", "Stepping Stars", "New Age"),
+    #     50: ("David Arkenstone", "Carnation Lily Lily Rose", "New Age"),
+    #     51: ("David Lanz", "Behind The Waterfall", "New Age"),
+    #     52: ("David Lanz", "Cristofori's Dream", "New Age"),
+    #     53: ("David Lanz", "Heartsounds", "New Age"),
+    #     54: ("David Lanz", "Leaves on the Seine", "New Age"),
+        }
+        musicdata = musicdata.items()
+        musicdata.sort()
+        musicdata = [[str(k)] + list(v) for k,v in musicdata]
+        return musicdata
 #---------------------------------------------------------------------------
 if __name__ == '__main__':
     app = wx.App(False)
     frame = wx.Frame(None)
     
     
-    # Get the data from the ListCtrl sample to play with, converting it
-    # from a dictionary to a list of lists, including the dictionary key
-    # as the first element of each sublist.
-    musicdata = {
-    1 : ("Bad English", "The Price Of Love", "Rock"),
-    2 : ("DNA featuring Suzanne Vega", "Tom's Diner", "Rock"),
-    3 : ("George Michael", "Praying For Time", "Rock"),
-    4 : ("Gloria Estefan", "Here We Are", "Rock"),
-    5 : ("Linda Ronstadt", "Don't Know Much", "Rock"),
-    6 : ("Michael Bolton", "How Am I Supposed To Live Without You", "Blues"),
-    7 : ("Paul Young", "Oh Girl", "Rock"),
-    8 : ("Paula Abdul", "Opposites Attract", "Rock"),
-    9 : ("Richard Marx", "Should've Known Better", "Rock"),
-    10: ("Rod Stewart", "Forever Young", "Rock"),
-    11: ("Roxette", "Dangerous", "Rock"),
-    12: ("Sheena Easton", "The Lover In Me", "Rock"),
-    13: ("Sinead O'Connor", "Nothing Compares 2 U", "Rock"),
-    14: ("Stevie B.", "Because I Love You", "Rock"),
-    15: ("Taylor Dayne", "Love Will Lead You Back", "Rock"),
-    16: ("The Bangles", "Eternal Flame", "Rock"),
-    17: ("Wilson Phillips", "Release Me", "Rock"),
-    18: ("Billy Joel", "Blonde Over Blue", "Rock"),
-    19: ("Billy Joel", "Famous Last Words", "Rock"),
-    20: ("Janet Jackson", "State Of The World", "Rock"),
-    21: ("Janet Jackson", "The Knowledge", "Rock"),
-    22: ("Spyro Gyra", "End of Romanticism", "Jazz"),
-    23: ("Spyro Gyra", "Heliopolis", "Jazz"),
-    24: ("Spyro Gyra", "Jubilee", "Jazz"),
-    25: ("Spyro Gyra", "Little Linda", "Jazz"),
-    26: ("Spyro Gyra", "Morning Dance", "Jazz"),
-    27: ("Spyro Gyra", "Song for Lorraine", "Jazz"),
-    28: ("Yes", "Owner Of A Lonely Heart", "Rock"),
-    29: ("Yes", "Rhythm Of Love", "Rock"),
-    30: ("Billy Joel", "Lullabye (Goodnight, My Angel)", "Rock"),
-    31: ("Billy Joel", "The River Of Dreams", "Rock"),
-    32: ("Billy Joel", "Two Thousand Years", "Rock"),
-    33: ("Janet Jackson", "Alright", "Rock"),
-    34: ("Janet Jackson", "Black Cat", "Rock"),
-    35: ("Janet Jackson", "Come Back To Me", "Rock"),
-    36: ("Janet Jackson", "Escapade", "Rock"),
-    37: ("Janet Jackson", "Love Will Never Do (Without You)", "Rock"),
-    38: ("Janet Jackson", "Miss You Much", "Rock"),
-    39: ("Janet Jackson", "Rhythm Nation", "Rock"),
-    40: ("Cusco", "Dream Catcher", "New Age"),
-    41: ("Cusco", "Geronimos Laughter", "New Age"),
-    42: ("Cusco", "Ghost Dance", "New Age"),
-    43: ("Blue Man Group", "Drumbone", "New Age"),
-    44: ("Blue Man Group", "Endless Column", "New Age"),
-    45: ("Blue Man Group", "Klein Mandelbrot", "New Age"),
-    46: ("Kenny G", "Silhouette", "Jazz"),
-    47: ("Sade", "Smooth Operator", "Jazz"),
-    48: ("David Arkenstone", "Papillon (On The Wings Of The Butterfly)", "New Age"),
-    49: ("David Arkenstone", "Stepping Stars", "New Age"),
-    50: ("David Arkenstone", "Carnation Lily Lily Rose", "New Age"),
-    51: ("David Lanz", "Behind The Waterfall", "New Age"),
-    52: ("David Lanz", "Cristofori's Dream", "New Age"),
-    53: ("David Lanz", "Heartsounds", "New Age"),
-    54: ("David Lanz", "Leaves on the Seine", "New Age"),
-    }
-    musicdata = musicdata.items()
-    musicdata.sort()
-    musicdata = [[str(k)] + list(v) for k,v in musicdata]
-    panel = ResultPanel(frame, data=musicdata)
+
+#     panel = ResultPanel(frame, data=musicdata)
+    panel = CreatingResultWithToolbarPanel(frame)
     frame.Show()
     app.MainLoop()
 
