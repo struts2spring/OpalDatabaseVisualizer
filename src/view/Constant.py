f62917a3fd24537130b0e30d38e5c952326340be
struts2spring <struts2spring@gmail.com>
2016-12-30 18:49:52 +0530

    Create Constant.py

---
+++ src/view/Constant.py
@@ -0,0 +1,16 @@
+import wx
+
+ID_UPDATE_CHECK = wx.NewId()
+ID_newConnection = wx.NewId()
+ID_openConnection = wx.NewId()
+ID_newWorksheet = wx.NewId()
+ID_RUN = wx.NewId()
+ID_EXECUTE_SCRIPT = wx.NewId()
+ID_ROW_ADD = wx.NewId()
+ID_ROW_DELETE = wx.NewId()
+ID_RESULT_NEXT = wx.NewId()
+ID_RESULT_PREVIOUS = wx.NewId()
+ID_RESULT_REFRESH = wx.NewId()
+ID_APPLY_CHANGE = wx.NewId()
+ID_RESULT_FIRST = wx.NewId()
+ID_RESULT_LAST = wx.NewId()
