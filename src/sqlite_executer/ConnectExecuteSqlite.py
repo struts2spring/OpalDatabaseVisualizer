371735efdb0a5c5de64f9a9204be88a51e8ea5db
struts2spring <struts2spring@gmail.com>
2017-02-22 22:09:21 +0530

    save

--- src/sqlite_executer/ConnectExecuteSqlite.py
+++ src/sqlite_executer/ConnectExecuteSqlite.py
@@ -7,14 +7,16 @@ import sqlite3
 import os
 import re
 from os.path import expanduser
+import sys
 
 class SQLExecuter():
     '''
     '''
-    def __init__(self, database=None):
+    def __init__(self, database='_opal.sqlite'):
         home = expanduser("~")
         databasePath = os.path.join(home, database)
         self.conn = sqlite3.connect(databasePath)
+        self.createOpalTables()
         
     def sqlite_insert(self, table, rows):
         for row in rows:
@@ -106,7 +108,7 @@ class SQLExecuter():
         '''
         '''
         sqlScript='''
-        CREATE TABLE  if not exists connections
+        CREATE TABLE  if not exists conns
           (
             id INTEGER PRIMARY KEY,
             connection_name TEXT,
@@ -142,6 +144,80 @@ class SQLExecuter():
             self.conn.rollback()
             raise e
 
+    def getObject(self):
+    
+        con = None
+        
+        try:
+#             self.connection = sqlite3.connect('_opal.sqlite')
+            
+            cur = self.conn.cursor()    
+            cur.execute('SELECT SQLITE_VERSION()')
+            
+            data = cur.fetchone()
+            
+            print("SQLite version: %s" % data)   
+            cur.execute("select tbl_name from sqlite_master where type='table';")
+            types = cur.execute("select distinct type from sqlite_master;").fetchall()
+            databaseList = list()
+            dbObjects = list()
+#             print types
+            for t in types:
+#                 print t[0], type(t)
+                tObjectArrayList = list()
+                query = "select tbl_name from sqlite_master where type='%s' order by tbl_name;" % t[0]
+                print(query)
+                tObjectList = cur.execute(query).fetchall()
+                tableColumnList = list()
+                for tObj in tObjectList:
+                    if t[0] == 'table' or t[0] == 'index':
+                        tableColumnsOrIndexesSql = "PRAGMA " + t[0] + "_info(%s);" % tObj[0]
+                        print(tableColumnsOrIndexesSql)
+                        tableColumnsOrIndexesList = cur.execute(tableColumnsOrIndexesSql).fetchall()
+#                         print objChildList
+                        tableColumnsOrIndexes = list()
+                        for objChild in tableColumnsOrIndexesList:
+                            tableColumnsOrIndexes.append(objChild)
+#                             print objChild
+                        tableColumnList.append([tObj[0], tableColumnsOrIndexes])
+                    if t[0] == 'view':
+                        tableColumnList.append([tObj[0], []])
+                        print('view')
+                        
+#                     if t[0] == 'index':
+#                         tablesHavingIndexesSql = "PRAGMA " + t[0] + "_info(%s);" % tObj[0]
+#                         tablesHavingIndexesList = cur.execute(tablesHavingIndexesSql).fetchall()
+#                         print tablesHavingIndexesSql
+#                         for tableHavingIndexes in tablesHavingIndexesList:
+#                             tableIndexesSql = "PRAGMA " + t[0] + "_list(%s);" % tObj[0]
+# #                         print objChildList
+#                         tableColumnsOrIndexes = list()
+#                         for objChild in tableColumnsOrIndexesList:
+#                             tableColumnsOrIndexes.append(objChild)
+                        
+#                         print tableColumnList
+#                 tObjectArrayList.append(tableColumnList)
+#                 print tObjectArrayList
+                dbObjects.append((t[0], tableColumnList))
+            print(dbObjects)
+#                 dbObjects.append(tObjectArrayList)
+#             print dbObjects
+#             print cur.fetchallDict()
+#             for row in cur.execute("select tbl_name from sqlite_master where type='table';"):
+#                 print row                
+            
+#             data = cur.fetchone()
+        except sqlite3.Error as e:
+            print("Error %s:" % e.args[0])
+            sys.exit(1)
+            
+        finally:
+            
+            if self.conn:
+                self.conn.close()
+        databaseList.append('database')
+        databaseList.append(dbObjects)
+        return databaseList
 if __name__ == "__main__":
     print('hi')
 #     sqlExecuter = SQLExecuter(database='_opal.sqlite')
