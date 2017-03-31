8a47d5b18c891ace5ed6c19228b3bc46d7caa9ef
struts2spring <struts2spring@gmail.com>
2017-03-31 16:55:41 +0530

    Update FindAndReplacePanel.py

--- src/view/findAndReplace/FindAndReplacePanel.py
+++ src/view/findAndReplace/FindAndReplacePanel.py
@@ -34,6 +34,8 @@ class CreatingFindAndReplacePanel(wx.Panel):
     def __init__(self, parent=None, *args, **kw):
         wx.Panel.__init__(self, parent, id=-1)
         self.parent = parent
+        self.findText=''
+        self.replaceText=''
         vBox = wx.BoxSizer(wx.VERTICAL)
         import  wx.lib.rcsizer  as rcs
         rowColumnSizer = rcs.RowColSizer()
@@ -43,11 +45,11 @@ class CreatingFindAndReplacePanel(wx.Panel):
         h2 = wx.BoxSizer(wx.HORIZONTAL)
         
         h1.Add((1, 1), -1, wx.ALL)  # this is a spacer
-        schemaNameLabel = wx.StaticText(self, -1, "Find:")
-        schemaNameText = wx.TextCtrl(self, -1, '', size=(400, -1))
+        schemaNameLabel = wx.StaticText(self, -1, "F&ind:               ")
+        schemaNameText = wx.TextCtrl(self, -1, self.findText, size=(400, -1))
         
         tableNameLabel = wx.StaticText(self, -1, "Replace with:")
-        tableNameText = wx.TextCtrl(self, -1, '', size=(400, -1))
+        tableNameText = wx.TextCtrl(self, -1, self.replaceText, size=(400, -1))
         
         h1.Add(schemaNameLabel, 0, wx.ALL, 2)
         h1.Add(schemaNameText, 0, wx.ALL, 2)
@@ -62,7 +64,7 @@ class CreatingFindAndReplacePanel(wx.Panel):
         scopeRadioBox = wx.RadioBox(self, -1, "Scope", (10, 10), wx.DefaultSize, ['All', 'Selected lines'], 1, wx.RA_SPECIFY_COLS)
 
         # first the static box
-        box_10 = wx.StaticBox(self, -1, 'Option')
+        box_10 = wx.StaticBox(self, -1, 'Options')
         cb1=wx.CheckBox(box_10, -1, "Case sensitive", (35, 40), (150, 20))
         cb2=wx.CheckBox(box_10, -1, "Wra&p search", (35, 60), (150, 20))
         cb3=wx.CheckBox(box_10, -1, "&Incremental", (35, 80), (150, 20))
@@ -88,19 +90,19 @@ class CreatingFindAndReplacePanel(wx.Panel):
 #         sizer.AddGrowableCol(1)
 
         self.findButton = wx.Button(self, -1, "Find", pos=(50, 20))
-#         self.Bind(wx.EVT_BUTTON, self.onFindClicked, self.findButton)
+        self.Bind(wx.EVT_BUTTON, self.onFindClicked, self.findButton)
         self.findButton.SetDefault()
         
         self.replaceButton = wx.Button(self, -1, "Replace", pos=(50, 20))
-#         self.Bind(wx.EVT_BUTTON, self.onFindClicked, self.findButton)
+        self.Bind(wx.EVT_BUTTON, self.onReplaceClicked, self.replaceButton)
         self.replaceButton.SetDefault()
         
         self.replaceFindButton = wx.Button(self, -1, "Replace/Fin&d", pos=(50, 20))
-#         self.Bind(wx.EVT_BUTTON, self.onFindClicked, self.findButton)
+        self.Bind(wx.EVT_BUTTON, self.onReplaceFindClicked, self.replaceFindButton)
         self.replaceFindButton.SetDefault()
         
         self.replaceAllButton = wx.Button(self, -1, "Replace &All", pos=(50, 20))
-#         self.Bind(wx.EVT_BUTTON, self.onFindClicked, self.findButton)
+        self.Bind(wx.EVT_BUTTON, self.onReplaceAllClicked, self.replaceAllButton)
         self.replaceAllButton.SetDefault()
         
         self.closeButton = wx.Button(self, -1, "Close", pos=(50, 20))
@@ -134,7 +136,25 @@ class CreatingFindAndReplacePanel(wx.Panel):
 #         sizer = wx.BoxSizer(wx.VERTICAL)
 #         sizer.Add(vBox, 1, wx.EXPAND , 0)
         self.SetSizer(vBox)
-        self.SetAutoLayout(True)        
+        self.SetAutoLayout(True)       
+        
+    def onFindClicked(self, event):
+        print('onFindClicked: '+ self.findText) 
+    def onReplaceClicked(self, event):
+        print('onReplaceClicked') 
+    def onReplaceFindClicked(self, event):
+        print('onReplaceFindClicked') 
+    def onReplaceAllClicked(self, event):
+        print('onReplaceAllClicked') 
+
+    def setFindText(self):
+        if not wx.TheClipboard.IsOpened():  # may crash, otherwise
+            do = wx.TextDataObject()
+            wx.TheClipboard.Open()
+            success = wx.TheClipboard.GetData(do)
+            wx.TheClipboard.Close()
+            if success:
+                self.findText.SetValue(do.GetText())
 #---------------------------------------------------------------------------
 
       
@@ -142,3 +162,4 @@ if __name__ == '__main__':
     app = wx.App(False)
     frame = CreatingFindAndReplaceFrame(None, 'Find / Replace')
     app.MainLoop()
+
